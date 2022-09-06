638e976e36df6191903ef2a15295275fb6892705
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-09-06 13:55:02 +0300

    Delete tasks

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,12 @@ const App: FC = () => {
     setTodoList([...todoList, newTask])
     setTask("")
     setDeadline(0)
+  }
+
+  const completeTask = (taskNameToDelete: string):void => {
+    setTodoList(todoList.filter((task) => {
+      return task.taskName != taskNameToDelete
+    }))
 
   }
 
@@ -44,7 +50,7 @@ const App: FC = () => {
       </div>
       <div className='todoList'>
         {todoList.map((task: ITask, key: number) => {
-          return <TodoTask key={key} task={task}/>
+          return <TodoTask key={key} task={task} completeTask={completeTask}/>
         })}
       </div>
     </div>
